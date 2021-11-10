3ec7a5f45b67b9084809ab532b36832c241068aa
Jesse Kela <jessekkela@gmail.com>
2021-11-10 23:32:12 +0200

    Updated and improved tests

--- Tests/ConverterTests.cs
+++ Tests/ConverterTests.cs
@@ -1,26 +1,42 @@
 using System;
-using Xunit;
 using System.IO;
+using Xunit;
 using VideoConverter;
 
 namespace Tests
 {
     public class ConverterTests
     {
-        [Theory]
-        [InlineData("Testvideos/example.mp4", VideoFormat.Webm)]
-        [InlineData("Testvideos/example2.webm", VideoFormat.Mp4)]
-        public void ConversionIsSuccessful(string file, string format)
-        {
-            var path = Path.Join(Environment.CurrentDirectory, "Testvideos");
-            var converter = new Converter(path);
-            converter.ConvertVideo(file, format).Wait();
+        private readonly string TestVideoPath = "Testvideos";
 
-            var outFile = Path.ChangeExtension(file, format);
+        [Theory]
+        [InlineData("example.mp4", VideoFormat.Webm)]
+        [InlineData("example2.webm", VideoFormat.Mp4)]
+        public void ConversionIsSuccessful(string inputFile, string outputFormat)
+        {
+            var inputFilePath = Path.Join(TestVideoPath, inputFile);
+            var outputFilePath = Path.Join(Environment.CurrentDirectory, TestVideoPath);
+
+            var converter = new Converter(outputFilePath);
+            converter.ConvertVideoAsync(inputFilePath, outputFormat).Wait();
+
+            var outFile = Path.ChangeExtension(inputFilePath, outputFormat);
             var fileExists = File.Exists(outFile);
 
             Assert.True(fileExists);
             File.Delete(outFile);
+        }
+
+        [Fact]
+        public void DownloadIsSuccessful()
+        {
+            var url = "https://tinyurl.com/yw6vak3d";
+            var downloadedFile = Utility.DownloadFileAsync(url).Result;
+
+            var fileExists = File.Exists(downloadedFile);
+
+            Assert.True(fileExists);
+            File.Delete(downloadedFile);
         }
 
         [Theory]
