277d8c621593ce898787fbac36b67f73f6d8fafb
Jesse Kela <jessekkela@gmail.com>
2021-11-20 00:16:23 +0200

    Added new exception

--- VideoConverter/src/Converter/Exceptions.cs
+++ VideoConverter/src/Converter/Exceptions.cs
@@ -19,6 +19,23 @@ namespace VideoConverter
         }
     }
 
+    public class FFmpegPathException : Exception
+    {
+        public FFmpegPathException()
+        {
+        }
+
+        public FFmpegPathException(string message)
+            : base(message)
+        {
+        }
+
+        public FFmpegPathException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
     public class VideoFormatException : Exception
     {
         public VideoFormatException()
