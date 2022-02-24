be8c043f987fd2537e50209eed3dc031c881d253
Jesse Kela <jessekkela@gmail.com>
2022-02-24 21:00:09 +0200

    Simplified file with modern namespace declaration

--- FFmpegDownloader/Program.cs
+++ FFmpegDownloader/Program.cs
@@ -2,22 +2,20 @@ using System;
 using System.Threading.Tasks;
 using Xabe.FFmpeg.Downloader;
 
+namespace FFmpeg.Downloader;
 
-namespace FFmpeg.Downloader
+public class Program
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main()
-        {
-            var path = Environment.CurrentDirectory;
-            Console.WriteLine($"Downloading FFmpeg executables to {path}");
-            DownloadFFmpegExecutables(path).Wait();
-            Console.WriteLine("Download finished.");
-        }
+        var path = Environment.CurrentDirectory;
+        Console.WriteLine($"Downloading FFmpeg executables to {path}");
+        DownloadFFmpegExecutables(path).Wait();
+        Console.WriteLine("Download finished.");
+    }
 
-        private async static Task DownloadFFmpegExecutables(string destinationPath)
-        {
-            await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Full, destinationPath);
-        }
+    private async static Task DownloadFFmpegExecutables(string destinationPath)
+    {
+        await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Full, destinationPath);
     }
 }
