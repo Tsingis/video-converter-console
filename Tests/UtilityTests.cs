2164370628cf1cf1460017a6b2c556fcff99abba
Jesse Kela <jessekkela@gmail.com>
2024-08-02 15:17:55 +0300

    Mock download tests

--- Tests/UtilityTests.cs
+++ Tests/UtilityTests.cs
@@ -1,23 +1,72 @@
 using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
-using Xunit;
 using FluentAssertions;
+using Moq;
+using Moq.Protected;
 using VideoConverter;
+using Xunit;
 
 namespace Tests;
 
 public class UtilityTests
 {
-    [Theory]
-    [InlineData("https://github.com/Tsingis/video-converter-console/raw/main/Tests/Testvideos/example_mp4.mp4")]
-    [InlineData("https://github.com/Tsingis/video-converter-console/raw/main/Tests/Testvideos/example_webm.webm")]
-    [InlineData("https://github.com/Tsingis/video-converter-console/raw/main/Tests/Testvideos/example_gif.gif")]
-
-    public async Task TestDownload(string url)
+    [Fact]
+    public async Task TestDownloadOk()
     {
+        var url = "https://someurl.com/video.mp4";
+        var content = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        var mockHandler = new Mock<HttpClientHandler>();
+
+        mockHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new ByteArrayContent(content),
+            });
+
+        var httpClient = new HttpClient(mockHandler.Object);
+        Utility.HttpClientFactory = () => httpClient;
+
         var downloadedFile = await Utility.DownloadFileAsync(url);
+
         File.Exists(downloadedFile).Should().BeTrue();
+
+        var downloadedContent = await File.ReadAllBytesAsync(downloadedFile);
+        downloadedContent.Should().Equal(content);
+
         File.Delete(downloadedFile);
+    }
+
+    [Fact]
+    public async Task TestDownload_Fail()
+    {
+        var url = "https://someurl.com/video.mp4";
+        var mockHandler = new Mock<HttpClientHandler>();
+
+        mockHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.NotFound,
+            });
+
+        var httpClient = new HttpClient(mockHandler.Object);
+        Utility.HttpClientFactory = () => httpClient;
+
+        var exception = await Assert.ThrowsAsync<HttpRequestException>(async () => await Utility.DownloadFileAsync(url));
+        exception.Message.Should().Be("Download failed.");
+        exception.InnerException.Should().BeOfType<HttpRequestException>();
+        exception.InnerException.Message.Should().Contain("Status code: NotFound");
     }
 
     [Theory]
